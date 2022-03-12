17d27d9e2b333147fdf7ac3e4a0ad195602083b5
sean <midruss@yahoo.com>
2022-03-12 18:36:51 -0500

    try to update

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User")
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello Suckers!"));
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: "Jim",
+        email: "jim@jim.com",
+        password: "123456"
+    })
+    user.save()
+    res.send("Hello Suckers!")
+});
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
