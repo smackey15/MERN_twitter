2891ab962cd735ce6b380a96cab3a3650d34880c
sean <midruss@yahoo.com>
2022-03-13 11:46:28 -0400

    successfully test all tweet routes with postman

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
-const Tweet = require("../../models/Tweet");
-const validateTweetInput = require("../../validation/tweets");
+const Tweet = require('../../models/Tweet');
+const validateTweetInput = require('../../validation/tweets');
 
 // all tweets:
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
     Tweet.find()
-        .sort({date: -1})
+        .sort({ date: -1 })
         .then(tweets => res.json(tweets))
-        .catch(err => res.status(404).json({notweetsfound: "No tweets found"}));
+        .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
 });
 
 // single user's tweets:
@@ -24,28 +24,28 @@ router.get("/user/:user_id", (req, res) => {
 router.get("/:id", (req, res) => {
     Tweet.findById(req.params.id)
         .then(tweet => res.json(tweet))
-        .catch(err => res.status(404).json({notweetfound: "No tweetn found with that ID"}))
+        .catch(err => res.status(404).json({notweetfound: "No tweet found with that ID"}))
 })
 
 // create tweet on protected route:
-router.post("/",
-    passport.authenticate("jwt", { session: false }),
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
     (req, res) => {
-        const { errors, isValid } = validateTweetInput(req.body);
-
-        if (!isValid) {
-            return res.status(400).json(errors);
-        }
-
-        const newTweet = new Tweet({
-            test: req.body.text,
-            user: req.user.id
-        });
-
-        newTweet.save().then(tweet => res.json(tweet));
+      const { errors, isValid } = validateTweetInput(req.body);
+  
+      if (!isValid) {
+        return res.status(400).json(errors);
+      }
+  
+      const newTweet = new Tweet({
+        text: req.body.text,
+        user: req.user.id
+      });
+  
+      newTweet.save().then(tweet => res.json(tweet));
     }
-)
-
+  );
+  
 // router.get("/test", (req, res) => res.json({
 //     msg: "This is the tweets route"
 // }));
