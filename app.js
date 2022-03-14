a71bb1fa0a1fd37863038c9858b3ff2f2adab638
sean <midruss@yahoo.com>
2022-03-14 11:54:25 -0400

    corrected config/keys filepath

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
-const db = require('../config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
