4a6c150a6ed2c9f477e30f99e243cd6c81637a07
sean <midruss@yahoo.com>
2022-03-14 08:15:32 -0400

    moved code

--- app.js
+++ app.js
@@ -11,17 +11,19 @@ const User = require("./models/User")
 const passport = require("passport");
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
+  
 
 // app.get("/", (req, res) => {
 //     res.send("Hello Suckers!")
